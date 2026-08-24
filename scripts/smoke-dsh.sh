fc7ccf08d502134e5a7ad167035d4810a6d54fba
Max Hsu <maxmilian@gmail.com>
2026-08-24 17:01:41 +0800

    test: verify DSH web profile boot

--- scripts/smoke-dsh.sh
+++ scripts/smoke-dsh.sh
@@ -12,6 +12,7 @@ test -f "$tarball"
 
 DSH_HOME="$smoke_root/dsh-home" dsh plugin --profile web add "$tarball"
 DSH_HOME="$smoke_root/dsh-home" dsh --profile web --dump-config >"$smoke_root/config.yml"
+DSH_HOME="$smoke_root/dsh-home" dsh --profile web --help >"$smoke_root/help.txt"
 
 grep --fixed-strings --quiet 'dsh-forge' "$smoke_root/dsh-home/profiles/web/package.json"
 grep --fixed-strings --quiet 'forge-tools' "$smoke_root/config.yml"
